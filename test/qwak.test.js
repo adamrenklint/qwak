47685b0f554fee74134c50e2f7ae9b912bdbbbe6
Adam Renklint <adam@renklint.com>
2015-08-22 21:12:51 +0200

    fix bug in jump commands introduced in 0.3.1

--- test/qwak.test.js
+++ test/qwak.test.js
@@ -720,6 +720,20 @@ describe('qwak', function () {
 
     testCommand('/W_*/qiai', function (context) {
       var seq = context.sequences[0];
+      it('should be the correct length', function () {
+        expect(seq.notes.length).to.equal(4);
+      });
+      assertNote(seq.notes, 0, 0, '1.1.01', 'W');
+      assertNote(seq.notes, 1, 0, '1.2.01', 'W');
+      assertNote(seq.notes, 2, 0, '1.3.01', 'W');
+      assertNote(seq.notes, 3, 0, '1.4.01', 'W');
+    });
+
+    testCommand('/W_*', function (context) {
+      var seq = context.sequences[0];
+      it('should be the correct length', function () {
+        expect(seq.notes.length).to.equal(4);
+      });
       assertNote(seq.notes, 0, 0, '1.1.01', 'W');
       assertNote(seq.notes, 1, 0, '1.2.01', 'W');
       assertNote(seq.notes, 2, 0, '1.3.01', 'W');
